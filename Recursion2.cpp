c0890281f83397c5c67d37941f957a0215b09f8a
AbdulQuadir <96329368+cdxgh@users.noreply.github.com>
2023-11-02 11:26:46 +0530

    Add files via upload

---
+++ Recursion2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h> 
+using namespace std;
+void reverse(string& s, int i, int j){
+	if(i>j) return;
+
+	swap(s[i], s[j]);
+	i++;
+	j--;
+	reverse(s, i, j);
+}
+int main(){
+	string name = "Abdul";
+	reverse(name,0, name.length()-1);
+	cout<<name<<endl;
+	return 0;
+}
